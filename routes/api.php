c376bc77be44b7a7c9cb07c7a85b2a288bea54ed
zm20200109 <147525494+zm20200109@users.noreply.github.com>
2024-01-20 11:53:48 +0100

    adding authentication routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AgencijaController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AranzmanController;
 
+use App\Http\Controllers\API\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,6 +26,43 @@ Route::get('/agencije', [AgencijaController::class, 'index']);
 Route::get('agencija/{id}', [AgencijaController::class, 'show']);
 Route::put('azuriraj_agenciju/{id}', [AgencijaController::class, 'update']);
 Route::post('sacuvaj_agenciju', [AgencijaController::class, 'store']);
+
+
+
+Route::get('/users', [UserController::class, 'index']);
+//************************************
+
+//resource rute
+Route::resource('aranzmani', AranzmanController::class); //rutiranje preko kontrolera
+
+//registracija
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+    
+    Route::resource('aranzmani', AranzmanController::class)->only(['store','destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Route::get('/users', [UserController::class, 'index']);
 //************************************
