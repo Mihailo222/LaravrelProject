c00343c416d265d1780988b02d26b1d5560fc78d
Mihailo222 <mahovacmihailo@gmail.com>
2024-01-20 10:46:18 +0100

    adding api routes:GET, PUT, POST

--- app/Models/Agencija.php
+++ app/Models/Agencija.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Agencija;
 
 class Agencija extends Model
 {
     use HasFactory;
 
+    protected $table='agencija';
 
 
     protected $fillable = [
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgencijaController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+
+Route::get('/agencije', [AgencijaController::class, 'index']);
+Route::get('agencija/{id}', [AgencijaController::class, 'show']);
+Route::put('azuriraj_agenciju/{id}', [AgencijaController::class, 'update']);
+Route::post('sacuvaj_agenciju', [AgencijaController::class, 'store']);
+
+Route::get('/users', [UserController::class, 'index']);
