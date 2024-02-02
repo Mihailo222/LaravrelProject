024ff8e523f29e95bec5825648dc8857ac67beae
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-02-02 18:29:57 +0100

    Update AgencijaFactory

--- database/factories/AgencijaFactory.php
+++ database/factories/AgencijaFactory.php
@@ -3,12 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use \App\Models\Agencija;
 
+use \Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Agencija>
  */
 class AgencijaFactory extends Factory
 {
+
+       protected $model=\App\Models\Agencija::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,10 @@ class AgencijaFactory extends Factory
     public function definition()
     {
         return [
-            //
+              'naziv'=>$this->faker->company,
+            'adresa'=>$this->faker->address,
+            'telefon'=>$this->faker->phoneNumber,
+            'gmail'=>$this->faker->email,
         ];
     }
 }
